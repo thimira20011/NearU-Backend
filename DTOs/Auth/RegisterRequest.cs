bd47b504080a5dd5314c9d77fca1b4fffb68b9f7
pamal pahasara <pamalpahasara@gmail.com>
2026-03-12 14:11:24 +0530

    add registration, login DTOs and authresponce DTO for return token and user info

--- DTOs/Auth/RegisterRequest.cs
+++ DTOs/Auth/RegisterRequest.cs
@@ -2,5 +2,9 @@
 {
     public class RegisterRequest
     {
+        public string Id { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
