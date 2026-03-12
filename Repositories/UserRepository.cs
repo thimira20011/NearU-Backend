7cdd0cd1aef3e48364e892f2c078097890c6d9bb
pamal pahasara <pamalpahasara@gmail.com>
2026-03-13 00:18:24 +0530

    database operations for create users , getusersbyemail

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,6 +1,30 @@
-﻿namespace NearU_Backend_Revised.Repositories
+﻿using NearU_Backend_Revised.Services;
+using Microsoft.EntityFrameworkCore;
+using NearU_Backend_Revised.Models;
+using NearU_Backend_Revised.Data;
+
+
+namespace NearU_Backend_Revised.Repositories
 {
     public class UserRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
     }
 }
+
