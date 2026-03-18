677a9339af62733ee70a6ca169f184e109e57f45
pamal pahasara <pamalpahasara@gmail.com>
2026-03-18 11:31:59 +0530

    typing mistake and , missing

--- Models/ApiResponse.cs
+++ Models/ApiResponse.cs
@@ -3,23 +3,13 @@ namespace NearU_Backend_Revised.Models
     public class ApiResponse<T>
     {
         public bool Success { get; set; }
-        public string Message { get; set; }
-        public T Data { get; set }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
 
-        public static ApiResponse<string message, T data> =>
-            new ApiResponse<T>
-            {
-                Success = true,
-                Message = message,
-                Data = data
-            };
+        public static ApiResponse<T> SuccessResponse(string message, T data) =>
+            new ApiResponse<T> { Success = true, Message = message, Data = data };
 
-        public static FailResponse(string message) =>
-            new ApiResponse<T>
-            {
-                Success = false,
-                Message = message,
-                Data = default
-            };
+        public static ApiResponse<T> FailResponse(string message) =>
+            new ApiResponse<T> { Success = false, Message = message, Data = default };
     }
 }
