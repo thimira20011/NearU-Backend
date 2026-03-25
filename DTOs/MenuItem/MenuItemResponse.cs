1faf066b80cdd3fe9517db215ac3b296fc9d2bf2
pamal pahasara <pamalpahasara@gmail.com>
2026-03-25 15:05:20 +0530

    DTOs for food item create update and response

---
+++ DTOs/MenuItem/MenuItemResponse.cs
@@ -0,0 +1,19 @@
+namespace NearU_Backend_Revised.DTOs.FoodShop
+{
+    public class FoodShopResponse
+    {
+        public int Id { get; set; } = null!;
+
+        public int FoodShopId { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public DateTime CreateAt { get; set; }
+    }
+}
