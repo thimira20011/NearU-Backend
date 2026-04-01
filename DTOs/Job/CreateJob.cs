576c53465cae100c87f30c6df459d6340295b815
Thimira Niranjaya <Thimira Niranjaya>
2026-04-01 16:45:30 +0530

    DTOs for the Models Creation

---
+++ DTOs/Job/CreateJob.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NearU_Backend_Revised.DTOs.Job
+{
+    public class CreateJob
+    {
+        [Required(ErrorMessage = "Job title is required")]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = "Company name is required")]
+        public string Company { get; set; } = null!;
+
+        [Required(ErrorMessage = "Location  is required")]
+        public string Location { get; set; } = null!;
+
+        [Required(ErrorMessage = "Pay information is required")]
+        public string PayRange { get; set; } = null!;
+
+        [Required(ErrorMessage = "Job type is required")]
+        public string JobType { get; set; } = null!;
+
+        [Required(ErrorMessage = "Job category is required")]
+        public string Category { get; set; } = null!;
+
+        public string Logo { get; set; }
+
+        [Required(ErrorMessage = "Job description is required")]
+        public string Description { get; set; } = null!;
+
+        public string? LongDescription { get; set; }
+
+        public List<string>? Requirements { get; set; }
+
+        public List<string>? Tags { get; set; }
+
+        public bool IsNew { get; set; } = true;
+    }
+}
